4ca6a95ed3ae82c5d96d5f6fdc4c22241712118d
leodar163 <leobgely@gmail.com>
2021-10-19 14:19:23 +0200

    Screeshake et Singleton

--- ProjetUnity1erSem/Assets/Scripts/CameraMan.cs
+++ ProjetUnity1erSem/Assets/Scripts/CameraMan.cs
@@ -1,19 +1,35 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(Camera))]
 public class CameraMan : MonoBehaviour
 {
-    
+
+    private static CameraMan cela;
+
+    public static CameraMan Singleton
+    {
+        get
+        {
+            if (!cela) cela = FindObjectOfType<CameraMan>();
+            return cela;
+        }
+    }
+    [Header("Refs")]
+    [SerializeField] private Camera camera;
     [SerializeField] private GameObject objetASuivre;
+    [Header("Propriétés Caméra")]
     [SerializeField] private float tmpsPrAttendreObjet;
     [SerializeField] private float vitesseMax;
     [SerializeField] private float decalageVertical;
     [SerializeField] private float decalageHorizontal;
-    [SerializeField] private Camera camera;
+    [Header("Rendu")]
     [SerializeField] private float dezoom;
+    
     public float Dezoom
     {
         get => dezoom;
@@ -44,6 +60,8 @@ public class CameraMan : MonoBehaviour
     private void Update()
     {
         if(objetASuivre && fileRendu == FileRendu.Update) SuivreObjetASuivre();
+        
+        if(Input.anyKeyDown) ScreenShake(0.05f,0.2f);
     }
 
     private void FixedUpdate()
@@ -67,5 +85,34 @@ public class CameraMan : MonoBehaviour
 
         nvllePos.z = transform.position.z;
         transform.position = nvllePos;
+    }
+
+    private IEnumerator screenShake;
+    public void ScreenShake(float force, float duree)
+    {
+        if(screenShake != null) StopCoroutine(screenShake);
+        screenShake = ScreenShakeRoutine(force, duree);
+        StartCoroutine(screenShake);
+    }
+
+    private IEnumerator ScreenShakeRoutine(float force, float duree)
+    {
+        float tmps = duree;
+        while (tmps > 0)
+        {
+            Vector3 nvllePos = transform.position;
+            nvllePos.x += Random.Range(-1f, 1f) * force;
+            nvllePos.y += Random.Range(-1f, 1f) * force;
+            transform.position = nvllePos;
+            
+            float diffTmps = fileRendu == FileRendu.Update ? Time.deltaTime : Time.fixedDeltaTime;
+
+            if (fileRendu == FileRendu.Update) yield return new WaitForEndOfFrame();
+            else yield return new WaitForFixedUpdate();
+
+            tmps -= diffTmps;
+        }
+
+        screenShake = null;
     }
 }
