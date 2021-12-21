a6d44c50dab4b49e13d9c0e6acd894ace6d721dc
Lullamoon <elzazou50@gmail.com>
2021-12-21 17:40:28 +0100

    changements

--- ProjetUnity1erSem/Assets/Scripts/Acteurs/Coxi/Coxinelle.cs
+++ ProjetUnity1erSem/Assets/Scripts/Acteurs/Coxi/Coxinelle.cs
@@ -1,10 +1,5 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Security.Cryptography;
-using System.Threading;
-using UnityEditorInternal;
 using UnityEngine;
 
 public class Coxinelle : MonoBehaviour
